d6e9ecda6a5acf4c6c4a4a3cc5f1d3c6ab1ac85a
rrrossettiii <rrrossettiii@gmail.com>
2020-11-09 23:02:28 -0500

    updated gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // MYSQL;
 // =============*
-const mysql = require("mysql");
+var mysql = require("mysql");
 // -connection;
-const connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: process.env.LOCAL_PASSWORD,
-	database: "",
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: process.env.LOCAL_PASSWORD,
+		database: "hmu73ykpml5lss96",
+	});
+}
 // -on connect;
 connection.connect(function (err) {
 	if (err) throw err;
